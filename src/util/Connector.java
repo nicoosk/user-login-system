f4013971956b221a8e8e0b50da9054236716bc99
Nicolas Huitrayao <nicolas.hui18@gmail.com>
2023-11-03 16:49:13 -0300

    user login funcionando

---
+++ src/util/Connector.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class Connector {
+	Connection c;
+	String url, user, pass;
+
+	public Connector(){
+	}
+
+	public Connector(String url){
+		this.url = url;
+	}
+	public Connector(String url, String user, String pass){
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+	}
+
+	public Connection getConnection() throws SQLException {
+		try{
+			c = DriverManager.getConnection(url, user, pass);
+			System.out.println("Connection successful");
+		} catch (SQLException e) {
+			System.out.println("Error while trying to connect to database: " + e);
+			c.close();
+		}
+		return c;
+	}
+	public Connection getConnection(String user, String pass) throws SQLException {
+		try {
+			c = DriverManager.getConnection(url, user, pass);
+			System.out.println("Connection successful");
+		} catch (SQLException e) {
+			System.out.println("Error while trying to connect to database: " + e);
+			c.close();
+		}
+		return c;
+	}
+	public Connection getConnection(String url, String user, String pass) throws SQLException {
+		try {
+			c = DriverManager.getConnection(url, user, pass);
+			System.out.println("Connection successful");
+		} catch (SQLException e) {
+			System.out.println("Error while trying to connect to database: " + e);
+			c.close();
+		}
+		return c;
+	}
+
+
+}
---
+++ src/util/Login.java
@@ -0,0 +1,40 @@
+package util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Login {
+
+	public boolean logWithUser(@NotNull Connection c, String user, String pass) {
+		boolean isUserInDB = false;
+		String sql = "SELECT username, pass FROM users;";
+		try (PreparedStatement pt = c.prepareStatement(sql)) {
+			ResultSet rs = pt.executeQuery();
+			isUserInDB = userLoggedIn(rs, user, pass);
+		} catch (SQLException e) {
+			System.out.println("Error while executing query: " + e.getMessage());
+		}
+		return isUserInDB;
+	}
+
+	private boolean userLoggedIn(@NotNull ResultSet rs, String user, String pass) {
+		ArrayList<String> userList = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				String userToCompare = rs.getString("username");
+				String passToCompare = rs.getString("pass");
+				if (userToCompare.equals(user) && passToCompare.equals(pass)) return true;
+			}
+		} catch (SQLException e) {
+			System.out.println("Error with user recovering: " + e);
+
+		}
+		return false;
+	}
+
+}
