d2de168343f95e1356a7e69757107b4c6cb24fc8
Nicolas Huitrayao <nicolas.hui18@gmail.com>
2023-11-08 09:17:47 -0300

    arreglando branch

--- src/data_manager/cipher/Encrypter.java
+++
@@ -1,5 +0,0 @@
-package data_manager.cipher;
-
-public class Encrypter {
-
-}
--- src/data_manager/handlePassCipher/Encrypt.java
+++ src/data_manager/handlePassCipher/Encrypt.java
@@ -1,61 +1,6 @@
 package data_manager.handlePassCipher;
 
-import org.jetbrains.annotations.NotNull;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Base64;
-
 public class Encrypt {
-	String key = "askjdhajsdh";
-	public Encrypt(){
-	}
-	public Encrypt(String key){
-		this.key = key;
-	}
-	public SecretKeySpec createKey(String key){
-		try {
-			byte [] bytes = key.getBytes(StandardCharsets.UTF_8);
-			MessageDigest md = MessageDigest.getInstance("SHA-1");
-			bytes = md.digest(bytes);
-			bytes = Arrays.copyOf(bytes, 16);
-			return new SecretKeySpec(bytes, "AES");
-		} catch(Exception e){
-			return null;
-		}
-	}
-	public String encodeString(@NotNull String pass){
-		try {
-			SecretKeySpec secretKeySpec = createKey(key);
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-			byte [] chain = Base64.getDecoder().decode(pass);
-			byte [] encryptedChain = cipher.doFinal(chain);
-			return Base64.getEncoder().encodeToString(encryptedChain);
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException |
-		         InvalidKeyException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	public String decryptString(String decrypt_pass){
-		try {
-			SecretKeySpec secretKeySpec = createKey(key);
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-			byte [] chain = Base64.getDecoder().decode(decrypt_pass);
-			byte [] decryptedChain = cipher.doFinal(chain);
-			return Base64.getEncoder().encodeToString(decryptedChain);
-		}catch (Exception e){
-			throw new RuntimeException(e);
-		}
-	}
+
 
 }
