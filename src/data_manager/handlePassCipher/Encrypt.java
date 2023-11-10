ff91d84f592c028e90c030e3d19795db2456f9e8
Nicolas Huitrayao <nicolas.hui18@gmail.com>
2023-11-10 17:55:55 -0300

    Guardando codificacion mediante cipher

--- src/data_manager/handlePassCipher/Encrypt.java
+++ src/data_manager/handlePassCipher/Encrypt.java
@@ -2,16 +2,11 @@ package data_manager.handlePassCipher;
 
 import org.jetbrains.annotations.NotNull;
 
-import javax.crypto.BadPaddingException;
+
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
@@ -26,9 +21,9 @@ public class Encrypt {
 		secretKey = createKey(key);
 	}
 
-	public SecretKeySpec createKey(String key){
+	public SecretKeySpec createKey(String key)throws RuntimeException{
 		try {
-			byte[] chain = key.getBytes();
+			byte[] chain = key.getBytes(StandardCharsets.UTF_8);
 			MessageDigest md = MessageDigest.getInstance("SHA-1");
 			chain = md.digest(chain);
 			chain = Arrays.copyOf(chain, 16);
@@ -37,18 +32,18 @@ public class Encrypt {
 			throw new RuntimeException();
 		}
 	}
-	public String encodeString(@NotNull String pass){
+	public String encodeString(@NotNull String pass) throws RuntimeException{
 		try {
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			byte[] chain = pass.getBytes();
+			byte[] chain = Base64.getEncoder().encode(pass.getBytes(StandardCharsets.UTF_8));
 			byte[] encrypted = cipher.doFinal(chain);
 			return new String(encrypted);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	public String decryptString(String to_decrypt){
+	public String decryptString(String to_decrypt) throws RuntimeException{
 		try {
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(Cipher.DECRYPT_MODE, secretKey);
