8a783c2e9ff3c40c479f425412aae685fe16b291
Nicolas Huitrayao <nicolas.hui18@gmail.com>
2023-11-10 18:07:37 -0300

    encriptacion base64 funcionando

--- src/data_manager/handlePassCipher/Encrypt.java
+++ src/data_manager/handlePassCipher/Encrypt.java
@@ -11,46 +11,21 @@ import java.util.Arrays;
 import java.util.Base64;
 
 public class Encrypt {
-	String key = "holamundo";
-	SecretKeySpec secretKey;
-	public Encrypt(){
-		secretKey = createKey(key);
-	}
-	public Encrypt(String key){
-		this.key = key;
-		secretKey = createKey(key);
-	}
 
-	public SecretKeySpec createKey(String key)throws RuntimeException{
-		try {
-			byte[] chain = key.getBytes(StandardCharsets.UTF_8);
-			MessageDigest md = MessageDigest.getInstance("SHA-1");
-			chain = md.digest(chain);
-			chain = Arrays.copyOf(chain, 16);
-			return new SecretKeySpec(chain, "AES");
-		} catch(Exception e){
-			throw new RuntimeException();
-		}
+	public Encrypt(){
 	}
 	public String encodeString(@NotNull String pass) throws RuntimeException{
 		try {
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
 			byte[] chain = Base64.getEncoder().encode(pass.getBytes(StandardCharsets.UTF_8));
-			byte[] encrypted = cipher.doFinal(chain);
-			return new String(encrypted);
+			return new String(chain);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
 	public String decryptString(String to_decrypt) throws RuntimeException{
 		try {
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-
 			byte[] chain = Base64.getDecoder().decode(to_decrypt);
-			byte[] decrypted = cipher.doFinal(chain);
-			return new String(decrypted);
+			return new String(chain);
 		}catch (Exception e){
 			throw new RuntimeException(e);
 		}
